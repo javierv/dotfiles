65de31551a7bb9246df83966f242fb9f47bf2677
Javier Martín <javier@elretirao.net>
2012-11-03 20:02:33 +0100

    No enlazamos open_project al instalar la configuración.

--- install.rb
+++ install.rb
@@ -72,7 +72,7 @@ private
   end
 
   def ignored_files
-    %w[. .. install.rb README.markdown .git .gitconfig .gitignore .gitmodules]
+    %w[. .. install.rb README.markdown .git .gitconfig .gitignore .gitmodules open_project]
   end
 
   def absolute_path(file)
