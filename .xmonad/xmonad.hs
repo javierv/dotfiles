5e897e7ec9ef23c1a752653c82faad5711a6be4f
Javier Martín <javier@elretirao.net>
2016-09-25 15:53:33 +0200

    Arranque de ibus al iniciar sesión.
    Así puedo cambiar rápidamente de teclados.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -13,6 +13,7 @@ main = do
   xmonad $ kde4Config {
       startupHook = do
         setWMName "LG3D"
+        spawn "ibus-daemon -drx"
     , workspaces = ["1:dev", "2:mail", "3:misc", "4:music"]
     , manageHook = ((className =? "krunner") >>= return . not --> manageHook kde4Config)
         <+> (kdeOverride --> doFloat)
